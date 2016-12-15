c6f1a350bc5ecba7add4ffa3b4270aea22aca4a7
Хомченко <ilya_khomchenko@outlook.com>
2016-12-15 12:17:21 +0300

    SizeChanged and Binding

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CounterApp.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace CounterApp.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPageViewModel ViewModel { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new MainPageViewModel();
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ActualWidth >= 500)
+            {
+                buttonStack.Orientation = Orientation.Horizontal;
+                button1.Style = button2.Style = button3.Style  = Resources["horizontalButtonStyle"] as Style;
+            }
+            else
+            {
+                buttonStack.Orientation = Orientation.Vertical;
+                button1.Style = button2.Style = button3.Style  = Resources["verticalButtonStyle"] as Style;
+            }
+        }
+    }
+}
