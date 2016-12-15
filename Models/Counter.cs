a8665cf96391c7de97226c27f6b130fd370fef33
Хомченко <ilya_khomchenko@outlook.com>
2016-12-15 12:17:06 +0300

    SizeChanged and Binding

---
+++ Models/Counter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CounterApp.Core;
+
+namespace CounterApp.Models
+{
+    public class Counter : BindableBase
+    {
+        private  int count;
+
+        public int Count
+        {
+            get { return count; }
+            set
+            {
+                SetProperty(ref count, value);
+            }
+        }
+        public void Increment()
+        {
+            Count++;
+        }
+       
+        public void Decrement()
+        {
+            Count--;
+        }
+    }
+}
