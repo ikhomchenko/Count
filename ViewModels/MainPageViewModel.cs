79959cd48c0eec95828130b3c8147fb048126271
Хомченко <ilya_khomchenko@outlook.com>
2016-12-15 12:17:15 +0300

    SizeChanged and Binding

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CounterApp.Models;
+
+namespace CounterApp.ViewModels
+{
+    public  class MainPageViewModel
+    {
+        public Counter Counter { get; set; }
+
+        public MainPageViewModel()
+        {
+            Counter = new Counter();
+        }
+    }
+   
+}
