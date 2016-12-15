f763948e71758f5f91d4b7d1cd3b623be61e8372
Хомченко <ilya_khomchenko@outlook.com>
2016-12-15 12:16:50 +0300

    SizeChanged and Binding

---
+++ Core/BindableBase.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CounterApp.Core
+{
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(property, value)) return;
+
+            property = value;
+            RaisePropertyChanged(propertyName);
+            
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new  PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
